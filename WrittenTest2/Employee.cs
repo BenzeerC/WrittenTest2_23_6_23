86a1ade7fe8a0c00f153b780daafa6cebb7d8e96
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-06-27 11:57:41 +0530

    Update with Enum

--- WrittenTest2/Employee.cs
+++ WrittenTest2/Employee.cs
@@ -11,17 +11,23 @@ namespace WrittenTest2
     //HRA- 40% of basic salary
     // TA – 20% of basic salary
     //PF -12 % of basic salary
+
+    public enum Dept { HR,Admin,Sales,Development,Testing};//Enum
+    public enum Designation { Admin,Employee,Manager};
+    public enum Task { New,Open,Closed,Reopen,Test,Bugs};
     public class Employee
     {
         private int employeeId, employeeBasicSalary;
         private string employeeName;
         private double hra, ta, pf,grossSalary;
+        private Dept dept;
 
-        public Employee(int employeeId,string employeeName,int employeeBasicSalary)
+        public Employee(int employeeId,string employeeName,int employeeBasicSalary,Dept dept)
         {
             this.employeeId = employeeId;
             this.employeeName = employeeName;
             this.employeeBasicSalary = employeeBasicSalary;
+            this.dept = dept;
         }
 
         public void CalculateSalary()
@@ -35,14 +41,18 @@ namespace WrittenTest2
 
         public override string ToString()
         {
-            return $"Employee Id:{employeeId}\nEmployee Name:{employeeName}\ngrossSalary:{grossSalary}";
+            return $"Employee Id:{employeeId}\nEmployee Name:{employeeName}\ngrossSalary:{grossSalary}\nDept:{dept}";
         }
 
         public static void Main(string[] args)
         {
-            Employee employee = new Employee(245, "Sangeeta", 55000);
+            Employee employee = new Employee(245, "Sangeeta", 55000,Dept.HR);
             employee.CalculateSalary();
             Console.WriteLine(employee.ToString());
+
+            Employee employee1 = new Employee(651,"Dinesh",69800,Dept.Development);
+            employee1.CalculateSalary();
+            Console.WriteLine(employee1.ToString());
         }
     }
 }
